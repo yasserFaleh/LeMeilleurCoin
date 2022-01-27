cd2db011fd1afae1d2c65fd26a9f8ed28c002292
yasser <falehyasser@gmail.com>
2022-01-27 20:15:58 +0100

    cors fix

--- src/main/java/fr/emse/spring/lemeilleurcoin/api/ProductController.java
+++ src/main/java/fr/emse/spring/lemeilleurcoin/api/ProductController.java
@@ -37,6 +37,7 @@ public class ProductController {
      * @param order
      * @return List<ProductDto>
      */
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping("/{category}")
     List<ProductDto> getProductsByCategoryAndTitle(@RequestParam (name="title",defaultValue = "")String title,@PathVariable Category category, @RequestParam(required = false,name="order",defaultValue = "desc") String order){
         if ( "ASC".equals(order) )
@@ -44,7 +45,7 @@ public class ProductController {
 
         return productDao.findAllByCategoryOrderByDateDesc(category).stream().map(ProductDto::new).collect(Collectors.toList());
     }
-
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping("/all")
     List<ProductDto> getProductsByTitle(@RequestParam (name="title",defaultValue = "")String title,@RequestParam (required = false,name="category")Category category ){
         if ( category == null){
@@ -53,7 +54,7 @@ public class ProductController {
             return productDao.findAllByTitleByCategory(category,title).stream().map(ProductDto::new).collect(Collectors.toList());
         }
     }
-
+    @CrossOrigin(origins = "http://localhost:8080")
     @PostMapping("create")
     public ProductDto create(@RequestBody ProductDto odto){
         Product product = null;
@@ -75,7 +76,7 @@ public class ProductController {
 
         return new ProductDto(product);
     }
-
+    @CrossOrigin(origins = "http://localhost:8080")
     @DeleteMapping("/{id}")
     public Boolean delete(@RequestParam(name = "pass",defaultValue = "") String pass,@PathVariable(name = "id") Long id){
         if ( "".equals(pass)   )
@@ -90,12 +91,12 @@ public class ProductController {
             return true;
         }
     }
-
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping("/own/{email}")
     public List<ProductDto> getOffersByEmail(@PathVariable("email") String email){
         return productDao.findAllByUser(email).stream().map(ProductDto::new).collect(Collectors.toList());
     }
-
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping("/product/{id}")
     public ProductDto get(@PathVariable("id") Long id){
         Product product = productDao.getById(id);
@@ -103,7 +104,7 @@ public class ProductController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Not found");
         return new ProductDto(product);
     }
-
+    @CrossOrigin(origins = "http://localhost:8080")
     @PostMapping("modify")
     public ProductDto modify(@RequestParam("pass")String pass ,@RequestBody ProductDto odto){
         System.out.println(pass+ " "+ odto);
